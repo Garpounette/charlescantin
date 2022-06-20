be93c711ae7ac330d907458c231f438d0b766880
Christine Guillaume <christine@fguillaume.fr>
2022-06-20 15:45:16 +0200

    modification formulaire

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,45 +1,95 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.name]: event.value,
+  };
+};
 
 function MyForm() {
-  const [submitting, setSubmitting] = useState(false);
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submit, setSubmit] = useState(false);
   const handleSubmit = (e) => {
+    setSubmit(false);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formData }),
+    })
+      .then(setSubmit(true))
+      .catch((error) => alert(error));
     e.preventDefault();
-    setSubmitting(true);
-
-    setTimeout(() => {
-      setSubmitting(false);
-    }, 3000);
   };
-
-  return submitting ? (
-    <div className="MyForm__submitting">
-      Envoi du Formulaire en cours <br /> Veuillez patienter ...
-    </div>
+  const handleChange = (event) => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value,
+    });
+  };
+  return submit ? (
+    <div className="MyForm__submitting">Message envoyé</div>
   ) : (
     <Form className="MyForm row" onSubmit={handleSubmit}>
       <Form.Group className="MyForm__group">
         <Form.Label className="MyForm__label">Nom *</Form.Label>
-        <Form.Control className="MyForm__control" type="text" required name="lastname" />
+        <Form.Control
+          className="MyForm__control"
+          type="text"
+          required
+          name="lastname"
+          onChange={handleChange}
+        />
       </Form.Group>
       <Form.Group className="MyForm__group">
         <Form.Label className="MyForm__label">Prénom *</Form.Label>
-        <Form.Control className="MyForm__control" type="text" required name="firstname" />
+        <Form.Control
+          className="MyForm__control"
+          type="text"
+          required
+          name="firstname"
+          onChange={handleChange}
+        />
       </Form.Group>
       <Form.Group className="MyForm__group">
         <Form.Label className="MyForm__label">Téléphone</Form.Label>
-        <Form.Control className="MyForm__control" type="text" name="telephone" />
+        <Form.Control
+          className="MyForm__control"
+          type="text"
+          name="telephone"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group className="MyForm__group">
         <Form.Label className="MyForm__label">Email *</Form.Label>
-        <Form.Control className="MyForm__control" type="email" required name="email" />
+        <Form.Control
+          className="MyForm__control"
+          type="email"
+          required
+          name="email"
+          onChange={handleChange}
+        />
       </Form.Group>
 
       <Form.Group className="MyForm__group MyForm__group-message">
         <Form.Label className="MyForm__label">Votre message *</Form.Label>
-        <Form.Control className="MyForm__control" as="textarea" rows={8} required name="message" />
+        <Form.Control
+          className="MyForm__control"
+          as="textarea"
+          rows={8}
+          required
+          name="message"
+          onChange={handleChange}
+        />
         <Form.Text className="text-muted">* Champs obligatoires</Form.Text>
       </Form.Group>
 
