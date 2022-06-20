c7ab3647493febc8f3e6aba97a9de519916a58a7
Christine Guillaume <christine@fguillaume.fr>
2022-06-20 18:01:02 +0200

    ajout d un useEffect sur la navbar pour correction d'un bug sur le lien actif

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -1,10 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo-remove-bg.png";
+import { useState, useEffect } from "react";
 
 function MyNavBar({ page }) {
+  const location = useLocation();
+  const [active, setActive] = useState("/");
+
+  useEffect(() => {
+    setActive(location.pathname.toLowerCase());
+  }, [location]);
+
   const darkNavBar = page !== "/" ? "darkNavBar" : null;
   return (
     <Navbar collapseOnSelect expand="lg" className={"MyNavBar flux " + darkNavBar}>
@@ -16,17 +24,39 @@ function MyNavBar({ page }) {
         )}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} className="MyNavBar__link nav-link" to="/" href="/">
+          <Nav
+            activeKey={active}
+            onSelect={(selectedKey) => setActive(selectedKey)}
+            className="ms-auto"
+          >
+            <Nav.Link as={Link} eventKey="/" className="MyNavBar__link nav-link" to="/" href="/">
               Accueil
             </Nav.Link>
-            <Nav.Link as={Link} className="MyNavBar__link nav-link" to="/Galery" href="/Galery">
+            <Nav.Link
+              as={Link}
+              eventKey="/galery"
+              className="MyNavBar__link nav-link"
+              to="/Galery"
+              href="/Galery"
+            >
               Galerie
             </Nav.Link>
-            <Nav.Link as={Link} className="MyNavBar__link nav-link" to="/Services" href="/Services">
+            <Nav.Link
+              as={Link}
+              eventKey="/services"
+              className="MyNavBar__link nav-link"
+              to="/Services"
+              href="/Services"
+            >
               Prestations
             </Nav.Link>
-            <Nav.Link as={Link} className="MyNavBar__link nav-link" to="/Contact" href="/Contact">
+            <Nav.Link
+              as={Link}
+              eventKey="/contact"
+              className="MyNavBar__link nav-link"
+              to="/Contact"
+              href="/Contact"
+            >
               Contact
             </Nav.Link>
           </Nav>
